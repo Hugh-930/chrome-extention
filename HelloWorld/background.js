0eaf35dd7bd66c572a2aa852628f243c2cf19206
HUGH <hughhanderson@icloud.com>
2024-07-21 21:27:11 -0700

    overall change

---
+++ HelloWorld/background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+      const queryParameters = tab.url.split("?")[1];
+      const urlParameters = new URLSearchParams(queryParameters);
+      console.log(urlParameters);
+  }
+});
